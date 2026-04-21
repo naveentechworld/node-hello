3e0acae7cd750d82b8bb81318a764c65abe92504
naveentechworld <naveentechworld@gmail.com>
2026-04-21 17:34:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'hai 1234 !\n'
+  const msg = 'hai 12345 !\n'
   res.end(msg);
 });
 
